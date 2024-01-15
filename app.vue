b80f77b22082ac41ec6ad6c4c051abf28ba38236
AndreGallini <andrea.gallini01@libero.it>
2024-01-15 10:26:28 +0100

    add second setion

--- app.vue
+++ app.vue
@@ -11,17 +11,56 @@
       <p>01</p>
       <p>WHY</p>
       </div>
-      
       <div class="body_section">
-        <img src="" alt="">
+      <div  class="hero_img ">
+        <img src="./assets/image/riso_why.png" alt="">
+      </div>
+      <div class="content">
+        <section class="chapter">
+          <p>chapter</p>
+          <p>001</p>
+          <p>one</p>
+        </section>
+      </div>
+        
+
       </div>
     </div>
   </div>
 </template>
 <style>
+.chapter{
+  display: flex;
+  width: 70%;
+  justify-content: space-between;
+}
+.content{
+  width: 900px;
+  padding-left: 100px;
+  padding-top: 150px;
+}
+.hero_img{
+  position: relative;
+  background-image: url('./assets/image/foto_why_1.png') ;
+  width: 900px;
+  height: 100vh;
+  background-size: cover;
+}
+.hero_img img{
+    position: absolute;
+    bottom: 0;
+    left: 600px;
+    width: 600px;
+}
+.section{
+  display: flex;
+}
+.lateral_nav{
+  border: 1px solid black
+}
   .section_home{
     background-image: url('./assets/image/foto_hero.webp') ;
-    width: 1920px;
+    width: calc(100% - 80px);
     height: 100vh;
     background-size: cover;
     flex: 0 0 auto;
@@ -36,8 +75,9 @@
     white-space: nowrap;
   }
   .body_section{
-    width: 500px;
+    width: 1900px;
     flex: 0 0 auto;
+    display: flex;
   }
 </style>
 <script>
